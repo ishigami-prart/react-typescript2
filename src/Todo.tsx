ae7f34c9f3718948677a3c1c11c00d8a67e15538
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-06-27 18:51:54 +0900

    完了or未

---
+++ src/Todo.tsx
@@ -0,0 +1,5 @@
+export const Todo = (props) => {
+  const { title, userid, completed } = props;
+  const completeMark = completed ? "[完]" : "[未]";
+  return <p>{`${completeMark} ${title}(ユーザー${userid})`}</p>;
+};

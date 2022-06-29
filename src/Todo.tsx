cfb8341c32f880ecaa63a34995154d1fd6e84c30
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-06-29 09:01:19 +0900

    todotype

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,4 +1,9 @@
-export const Todo = (props) => {
+type TodoType = {
+  userId: number;
+  title: string;
+  completed: boolean;
+};
+export const Todo = (props: TodoType) => {
   const { title, userid, completed } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}(ユーザー${userid})`}</p>;

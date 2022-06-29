ebc49dbdfc1588948aaa1ddeb0b3c1a9ee800b5d
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-06-29 09:30:22 +0900

    FC

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,5 +1,6 @@
+import { VFC } from "react";
 import { TodoType } from "./types/todo";
-export const Todo = (props: Omit<TodoType, "id">) => {
+export const Todo: VFC<Omit<TodoType, "id">> = (props) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}(ユーザー${userId})`}</p>;

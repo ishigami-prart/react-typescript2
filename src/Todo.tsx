9555919fdd07d1e332620cb1f2075c4b0e5505f2
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-06-29 09:16:15 +0900

    Picks Omit

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,9 +1,5 @@
-type TodoType = {
-  userId: number;
-  title: string;
-  completed?: boolean;
-};
-export const Todo = (props: TodoType) => {
+import { TodoType } from "./types/todo";
+export const Todo = (props: Omit<TodoType, "id">) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}(ユーザー${userId})`}</p>;
